9c799ab53628c872e813b144904dfa9022577e12
Martin Cura <martincura@gmail.com>
2021-07-29 12:21:44 -0300

    feat(Button): add button for documentation

--- arturito/src/components/HomeSection/index.tsx
+++ arturito/src/components/HomeSection/index.tsx
@@ -1,3 +1,11 @@
+const Button = () => (
+  <a className="mx-auto" href='/'>
+    <div className={`inline-block px-4 py-2 rounded-lg text-white`}>
+      Documentación
+    </div>
+  </a>
+);
+
 const HomeSection = () => <p className="p-3 text-lg">¡Elegí una sección!</p>;
 
 export default HomeSection;
