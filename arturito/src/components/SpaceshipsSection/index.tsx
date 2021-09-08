f7f8c6dbf09469660f9277dcfcba46c2686e6f1c
nelson <nelsonsorio@hotmail.com>
2021-09-08 16:01:40 -0500

    test first commit branch Spaceships

---
+++ arturito/src/components/SpaceshipsSection/index.tsx
@@ -0,0 +1,50 @@
+import useSWR from 'swr';
+
+import { swGet } from '../../utils/fetcher';
+import Table from '../Table';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Climate',
+    dataIndex: 'climate',
+    key: 'climate',
+  },
+  {
+    title: 'Population',
+    dataIndex: 'population',
+    key: 'population',
+    render: (population: string) =>
+      parseInt(population)
+        ? parseInt(population).toLocaleString('es-AR')
+        : population,
+  },
+  {
+    title: 'Residents count',
+    dataIndex: 'residents',
+    key: 'residents_count',
+    render: (residents: string[]) => residents.length,
+  },
+];
+
+const Spaceships = () => {
+  const { data, error } = useSWR('/planets', swGet);
+
+  if (error) {
+    return <div className="px-2">Oh oh!</div>;
+  }
+  if (!data) {
+    return <div className="px-2">Loading...</div>;
+  }
+  return (
+    <div>
+      <Table columns={columns} data={data.results} /* :D */ />
+    </div>
+  );
+};
+
+export default Spaceships;
