f4f2f59bf64726549d348f8c297a1744d912a498
Federico Voloschin <fedevoloschin@gmail.com>
2022-01-27 15:53:44 -0300

    prueba de commit

--- arturito/src/index.tsx
+++ arturito/src/index.tsx
@@ -13,7 +13,7 @@ ReactDOM.render(
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
-);
+);   
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
