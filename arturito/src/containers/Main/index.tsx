3dc8a53e7d07e4abdbb0e51f9664078c85780ff3
DemonTwo <76266733+DemonTwo@users.noreply.github.com>
2022-06-28 20:22:16 -0300

    prettiered

--- arturito/src/containers/Main/index.tsx
+++ arturito/src/containers/Main/index.tsx
@@ -32,7 +32,7 @@ const MainContainer = () => {
         </Route>
 
         <Route path={paths.starships.href}>
-          <Starships/>
+          <Starships />
         </Route>
 
         <Route path={paths.people.href}>
