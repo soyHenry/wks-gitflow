bf87508a16ed2e1c0cae70c7e626d1e1e7c75f59
Yeison Villegas <yeisonvillegas07@gmail.com>
2022-06-30 11:23:26 -0500

    Primer commit desde la nueva rama

--- arturito/src/components/HomeSection/index.tsx
+++ arturito/src/components/HomeSection/index.tsx
@@ -10,6 +10,7 @@ const HomeSection = () => (
   <div className="p-3 space-y-3">
     <p className="text-lg">¡Elegí una sección!</p>
     <Button text="Documentación" href="https://www.swapi.it/documentation" />
+    <p>Cambio en nueva rama</p>
   </div>
 );
 
