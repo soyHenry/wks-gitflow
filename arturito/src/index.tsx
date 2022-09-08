8df47c670aeb15059dd337eecc422ba02ba5f5b0
thomneuhaus2 <thom.neuhaus2@gmail.com>
2022-09-08 12:53:26 -0300

    test2

--- arturito/src/index.tsx
+++ arturito/src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//index
+//index 2
 ReactDOM.render(
   <React.StrictMode>
     <Router>
