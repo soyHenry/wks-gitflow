de24863d2a9f9e28ac613ccb748b7b2d7702db23
unknown <luciofuentes55@gmail.com>
2022-11-01 19:19:34 -0300

    People

---
+++ arturito/src/components/PeopleSection/index.tsx
@@ -0,0 +1,51 @@
+import useSWR from 'swr';
+
+import { swGet } from '../../utils/fetcher';
+import Table from '../Table';
+
+const columns = [
+    {
+        title  : "Name",
+        dataIndex : "name",
+        key : "name",
+    },
+    {
+        title : "Birth year",
+        dataIndex:"birth_year",
+        key :"birth_year",
+    },
+    {
+        title : "Height",
+        dataIndex : "height",
+        key : "height",
+        render: (height: string) =>
+      parseInt(height)
+        ? parseInt(height).toLocaleString('es-AR')
+        : height,
+    },
+    {
+        title: "Films",
+        dataIndex : "films",
+        key : "films",
+        render:(films : string[]) => films.length,
+    }
+]
+
+const People = () => {
+    const { data, error } = useSWR('/people', swGet);
+  
+    if (error) {
+      return <div className="px-2">Oh oh!</div>;
+    }
+    if (!data) {
+      return <div className="px-2">Loading...</div>;
+    }
+  
+    return (
+      <div>
+        <Table columns={columns} data={data.results}/>
+      </div>
+    );
+  };
+
+  export default People
