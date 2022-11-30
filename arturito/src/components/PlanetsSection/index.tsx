2276f75b5024dbd8593836013b708672850c86ec
Matias <matiasdentoni@gmail.com>
2022-11-30 15:17:02 -0300

    primer commit

--- arturito/src/components/PlanetsSection/index.tsx
+++ arturito/src/components/PlanetsSection/index.tsx
@@ -49,7 +49,7 @@ const Planets = () => {
 
   return (
     <div>
-      <Table columns={columns} data={data.results.slice(0, 3)} /* :D */ />
+      <Table columns={columns} data={data.results.slice(0, 3)} /* :D   modificacion matias*/ />
     </div>
   );
 };
