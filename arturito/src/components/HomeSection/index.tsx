5b921e4f6079c0840af746302ee6914906fde2fc
L. Carolina Arias <joseluy123@gmail.com>
2023-01-29 18:07:18 -0500

    prueba2

--- arturito/src/components/HomeSection/index.tsx
+++ arturito/src/components/HomeSection/index.tsx
@@ -10,7 +10,7 @@ const HomeSection = () => (
   <div className="p-3 space-y-3">
     <p className="text-lg">¡Elegí una sección!</p>
     <Button text="Documentación" href="https://www.swapi.it/documentation" />
-    <p>Carlos</p>
+    <p>Carlos pert</p>
   </div>
 );
 
