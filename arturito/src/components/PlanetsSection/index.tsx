e651ffc811669808325516c67f03c90ec04980d5
jesus alejandro perez <jesuperez05@gmail.com>
2023-01-29 18:52:49 -0300

    eliminacion de slice en planetsSection

--- arturito/src/components/PlanetsSection/index.tsx
+++ arturito/src/components/PlanetsSection/index.tsx
@@ -43,7 +43,8 @@ const Planets = () => {
 
   return (
     <div>
-      <Table columns={columns} data={data.results.slice(0, 3)} /* :D */ />
+      <Table columns={columns} data={data.results} />
+      
     </div>
   );
 };
