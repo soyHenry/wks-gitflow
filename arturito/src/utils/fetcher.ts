3fcdd2ba136de403913321376d02de0a4dd6164f
luicho76 <luishg.ingeniero@gmail.com>
2023-01-30 09:53:04 -0400

    ejecucion para ver todos los planetas

--- arturito/src/utils/fetcher.ts
+++ arturito/src/utils/fetcher.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// Possible alternative: 'https://swapi.dev/..api'
+// Possible alternative: 'https://swapi.dev/api'
 const baseURL = 'https://swapi.dev/api';
 
 export const swGet = (url: string) =>
