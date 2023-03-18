8f6e1c2f6c00fdb8d90a06af342650bbfff06c2d
RocioAlday <roo.alday@gmail.com>
2023-03-18 17:27:57 -0300

    People route & component Adding

---
+++ arturito/src/components/People/index.tsx
@@ -0,0 +1,48 @@
+import useSWR from 'swr';
+
+import { swGet } from '../../utils/fetcher';
+import Table from '../Table';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Birth Year',
+    dataIndex: 'birth_year',
+    key: 'birth_year',
+  },
+  {
+    title: 'Height',
+    dataIndex: 'height',
+    key: 'height',
+    render: (height: string)=> Number(height)/100,
+  },
+  {
+    title: 'Films',
+    dataIndex: 'films',
+    key: 'films_count',
+    render: (films: string[]) => films.length,
+  },
+];
+
+const People = () => {
+  const { data, error } = useSWR('/people', swGet);
+
+  if (error) {
+    return <div className="px-2">Oh oh!</div>;
+  }
+  if (!data) {
+    return <div className="px-2">Loading...</div>;
+  }
+
+  return (
+    <div>
+      <Table columns={columns} data={data.results} /* :D */ />
+    </div>
+  );
+};
+
+export default People;
