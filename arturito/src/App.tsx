935ff3d0725eb6237222a6cfbb4f1c729d838766
Kim <kim@MacBook-Pro-de-Kim.local>
2023-03-21 21:56:45 -0300

    haciendo prueba nicolas

--- arturito/src/App.tsx
+++ arturito/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import MainContainer from './containers/Main';
+import Boton from './components/Boton/Botton';
 
 const App = () => (
   <div className="h-full w-full bg-gray-800 text-white">
     <Header />
 
     <MainContainer />
+    <Boton/>
   </div>
 );
 
---
+++ arturito/src/components/Boton/Botton.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Boton(){
+
+    return(
+      <div>
+        <h1>Hola! Prueba!</h1>
+      </div>
+    )
+}
