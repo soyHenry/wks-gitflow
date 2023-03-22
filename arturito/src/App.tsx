92203b41a266b740b5d501fff04b5b2293544ddc
Pedro pina <pedroopina20@gmail.com>
2023-03-21 21:52:25 -0300

    pipo change

--- arturito/src/App.tsx
+++ arturito/src/App.tsx
@@ -1,10 +1,11 @@
 import Header from './components/Header';
 import MainContainer from './containers/Main';
+import Home from './components/Home';
 
 const App = () => (
   <div className="h-full w-full bg-gray-800 text-white">
     <Header />
-
+    <Home />
     <MainContainer />
   </div>
 );
