068ffcc29afe4cddb9b66f64d79312ec1e0b8652
juanmavers <juanmanuelv@outlook.com>
2023-05-05 12:20:40 -0300

    planet & starship update!

--- arturito/src/utils/fetcher.ts
+++ arturito/src/utils/fetcher.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Possible alternative: 'https://swapi.dev/api'
-const baseURL = 'https://www.swapi.it/api';
+const baseURL = 'https://swapi.dev/api';
 
 export const swGet = (url: string) =>
   axios.get(url, { baseURL }).then((res) => res.data);
