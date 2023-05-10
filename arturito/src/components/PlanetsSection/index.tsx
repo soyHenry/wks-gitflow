c6445d593c665cba0bf39258f9827d01ae88ef15
juanlomdp <juanlorenzomdp@gmail.com>
2023-05-10 13:59:42 -0300

    fix (*arturito): replace api url // refactor(components/secction) :delete slice

--- arturito/src/components/PlanetsSection/index.tsx
+++ arturito/src/components/PlanetsSection/index.tsx
@@ -32,8 +32,8 @@ const columns = [
 ];
 
 const Planets = () => {
-  const { data, error } = useSWR('/planets', swGet);
-
+  const { data, error } = useSWR('/planets/', swGet);
+ 
   if (error) {
     return <div className="px-2">Oh oh!</div>;
   }
@@ -43,7 +43,7 @@ const Planets = () => {
 
   return (
     <div>
-      <Table columns={columns} data={data.results.slice(0, 3)} /* :D */ />
+      <Table columns={columns} data={data.results} /* :D */ />
     </div>
   );
 };
