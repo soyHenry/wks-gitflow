529fa1df5dd62a4676cb7d5639ef221f8444f05d
ignaciocenni <ignaziocenni@gmail.com>
2023-05-31 15:29:17 -0300

    add comment

--- arturito/src/utils/fetcher.ts
+++ arturito/src/utils/fetcher.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 // Possible alternative: 'https://swapi.dev/api'
-const baseURL = 'https://www.swapi.dev/api';
+const baseURL = 'https://www.swapi.it/api';
+
+// Holis
 
 export const swGet = (url: string) =>
   axios.get(url, { baseURL }).then((res) => res.data);
-
-//
