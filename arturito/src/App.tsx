6027941baeb8cfbc3c11f4f7dd8e453bc1f4a28f
Brian Gelvez <briannn97@gmail.com>
2023-07-27 20:25:59 -0300

    Components_StarshipsSelection

--- arturito/src/App.tsx
+++ arturito/src/App.tsx
@@ -1,10 +1,10 @@
 import Header from './components/Header';
 import MainContainer from './containers/Main';
 
+
 const App = () => (
   <div className="h-full w-full bg-gray-800 text-white">
     <Header />
-
     <MainContainer />
   </div>
 );
