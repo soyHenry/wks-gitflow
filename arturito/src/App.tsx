1f907e7d0e1311e8d2b6f06f3a19a69afd6e8fb6
CACornelio <corneliocarlos.a@gmail.com>
2023-07-29 13:31:19 -0600

    test24

--- arturito/src/App.tsx
+++ arturito/src/App.tsx
@@ -2,7 +2,7 @@ import Header from './components/Header';
 import MainContainer from './containers/Main';
 //comentario de prueba 3
 //Comentario de Adri
-
+//comentario de lucaw
 const App = () => (
   <div className="h-full w-full bg-gray-800 text-white">
     <Header />
