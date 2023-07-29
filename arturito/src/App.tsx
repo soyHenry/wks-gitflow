dc50bfd21c0ab3916959b1b6ced5b7d547851ccc
DiazAdriel0 <diazadriel0@gmail.com>
2023-07-29 16:03:04 -0300

    comment added

--- arturito/src/App.tsx
+++ arturito/src/App.tsx
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import MainContainer from './containers/Main';
 //comentario de prueba 3
-// Comentario de Adri
+// Comentario de Adri 
 
 const App = () => (
   <div className="h-full w-full bg-gray-800 text-white">
