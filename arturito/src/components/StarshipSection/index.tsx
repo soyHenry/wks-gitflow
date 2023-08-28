8e252a7752e8321735a31624eae047dc895a564e
samuelbernal44 <samuelbernal44@gmail.com>
2023-08-27 20:14:15 -0500

    Crear Componente Starship

---
+++ arturito/src/components/StarshipSection/index.tsx
@@ -0,0 +1,45 @@
+import useSWR from 'swr';
+import { swGet } from '../../utils/fetcher';
+import Table from '../Table';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Model',
+    dataIndex: 'model',
+    key: 'model',
+  },
+  {
+    title: 'Manufacturer',
+    dataIndex: 'manufacturer',
+    key: 'manufacturer',
+  },
+  {
+    title: 'Cost in Credits',
+    dataIndex: 'cost_in_credits',
+    key: 'cost_in_credits',
+  },
+];
+
+const Starships = () => {
+  const { data, error } = useSWR('/starships', swGet);
+
+  if (error) {
+    return <div className="px-2">Oh oh!</div>;
+  }
+  if (!data) {
+    return <div className="px-2">Loading...</div>;
+  }
+
+  return (
+    <div>
+      <Table columns={columns} data={data.results.slice(0, 36)} /* :D */ />
+    </div>
+  );
+};
+
+export default Starships;
